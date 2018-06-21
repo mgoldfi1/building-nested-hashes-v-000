b6b14b318a8796e7772a2ff6cb7ee707286a90d7
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-21 17:58:58 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,11 +2,11 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       patriarch: {}, matriarch: {}, hero: {}, hero_friends: []
-      
+
       },
     :capulet => {
       patriarch: {}, matriarch: {}, heroine: {}, heroine_friends: []
-  
+
       }
     }
 
